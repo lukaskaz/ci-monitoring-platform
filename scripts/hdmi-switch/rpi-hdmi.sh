087f2331d586acc2dc46933593a6e2b8555b6d4a
lukaskaz <lukask84@o2.pl>
2024-05-01 20:38:54 +0200

    Update rpi-hdmi.sh

--- scripts/hdmi-switch/rpi-hdmi.sh
+++ scripts/hdmi-switch/rpi-hdmi.sh
@@ -1,28 +1,38 @@
-#!/bin/sh
+#!/bin/bash
 
 # Enable and disable HDMI output on the Raspberry Pi
 
-is_off ()
+function _status()
 {
-	vcgencmd display_power | grep "display_power=0" >/dev/null
+        status=$(ddcutil getvcp d6|tr -s ' '|sed -r "s/^.*\(sl=(.*)\).*$/\1/g")
+        if [ -z $status ]; then
+                echo "Cannot check status"
+        elif [ $status = 0x01 ]; then
+                echo "Display is on"
+                return 1
+        elif [ $status = 0x05 ]; then
+                echo "Display is off"
+                return 0
+        fi
+        return 2
 }
 
 case $1 in
-	off)
-        	ddcutil setvcp d6 5	
-		#vcgencmd display_power 0
-	;;
-	on)
-		ddcutil setvcp d6 1
-		#vcgencmd display_power 1
-	;;
-	status)
-		ddcutil getvcp d6	
-	;;
-	*)
-		echo "Usage: $0 on|off|status" >&2
-		exit 2
-	;;
+        off)
+                _status &> /dev/null
+                if [ $? -eq 1 ]; then ddcutil setvcp d6 0x05; fi
+        ;;
+        on)
+                _status &> /dev/null
+                if [ $? -eq 0 ]; then ddcutil setvcp d6 0x01; fi
+        ;;
+        status)
+                _$1
+        ;;
+        *)
+                echo "Usage: $0 on|off|status" >&2
+                exit 2
+        ;;
 esac
 
 exit 0
