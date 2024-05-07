273f4d2e803baae43c7211ea489a39c4fa29d1f5
Łukasz Kaźmierczak <lucas.kazmierczak@gmail.com>
2024-05-08 00:04:57 +0200

    Updating website control script
    Signed-off-by: Łukasz Kaźmierczak <lucas.kazmierczak@gmail.com>

--- scripts/gitboard/control.sh
+++ scripts/gitboard/control.sh
@@ -5,6 +5,8 @@ DESKTOPFILE=~/Desktop/gitboard.desktop
 WEBBROWSER="Firefox"
 WEBSITENAME="GitBoard.io"
 WEBSITEREGEX="$WEBSITENAME.*$WEBBROWSER"
+NOWEBSITENAME="Server Not Found"
+NOWEBSITEREGEX="$NOWEBSITENAME.*$WEBBROWSER"
 OPER=$1
 PARAM=$2
 
@@ -30,11 +32,16 @@ function _isParamInMap
 
 function _runOnWindow
 {
-	local ACTION=$1
+	local WEBSITE=$1
+	local ACTION=$2
 	
 	echo "Running: DISPLAY=$DISPLAY xdotool search --onlyvisible \
---name $WEBSITEREGEX windowactivate key $ACTION"
-	WINDOW=$(xdotool search --onlyvisible --name "$WEBSITEREGEX")
+--name $WEBSITE windowactivate key $ACTION"
+	WINDOW=$(xdotool search --onlyvisible --name "$WEBSITE")
+	if [ -z "$WINDOW" ]; then
+		_refreshemptysite
+	fi
+	WINDOW=$(xdotool search --onlyvisible --name "$WEBSITE")
 	if [ -n "$WINDOW" ]; then
 		xdotool windowactivate "$WINDOW"
 		xdotool key "$ACTION"
@@ -43,12 +50,21 @@ function _runOnWindow
 	return 1
 }
 
+function _refreshemptysite
+{
+	local PARAM=$1
+	local ACTION="Ctrl+F5"
+
+	_runOnWindow "$NOWEBSITEREGEX" "$ACTION"
+	return $?
+}
+
 function _refreshcb
 {
 	local PARAM=$1
 	local ACTION="Ctrl+F5"
 
-	_runOnWindow $ACTION
+	_runOnWindow "$WEBSITEREGEX" "$ACTION"
 	return $?
 }
 
@@ -59,7 +75,7 @@ function _zoomcb
 
 	_isParamInMap ZOOMMAP $PARAM
 	if [ $? -ne 0 ]; then echo "[$FUNCNAME] Param '$PARAM' not supported"; exit 3; fi
-	_runOnWindow $ACTION${ZOOMMAP[$PARAM]}
+	_runOnWindow "$WEBSITEREGEX" "$ACTION${ZOOMMAP[$PARAM]}"
 	return $?
 }
 
@@ -68,7 +84,7 @@ function _restartcb
 	local PARAM=$1
 	local ACTION="Ctrl+F4"
 
-	_runOnWindow $ACTION
+	_runOnWindow "$WEBSITEREGEX" "$ACTION"
 	echo "Running: DISPLAY=$DISPLAY gio launch $DESKTOPFILE"
 	sleep 1
 	gio launch $DESKTOPFILE
